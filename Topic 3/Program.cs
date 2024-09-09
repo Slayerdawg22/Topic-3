4865f0ed6b5b1497f9cf29e4b06ce07ffcc5b121
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-09 09:31:26 -0400

    Add project files.

---
+++ Topic 3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Raine DeVries
+string myName, myEyes, myTeeth, myHair;
+int myAge, myHeight, myWeight;
+
+myName = "Raine DeVries";
+myAge = 14;
+myHeight = 69; //inches
+myWeight = 130; //pounds
+myEyes = "Brown";
+myTeeth = "White";
+myHair = "Black";
+
+Console.WriteLine("Let's talk about " + myName);
+
