3ef38770fc2a000d01f36bd8233fd5c1835b85fa
Raine DeVries <362571390@GoTVDSB.ca>
2024-09-10 08:56:10 -0400

    finished

--- Topic 3/Program.cs
+++ Topic 3/Program.cs
@@ -12,5 +12,32 @@ myTeeth = "White";
 myHair = "Black";
 
 Console.WriteLine("Let's talk about " + myName);
-Console.WriteLine("He's " + myHeight + " inches tall.");
-Console.WriteLine("He's " + myWeight + " pounds heavy.");
+Console.WriteLine("He's " + (myHeight*0.083) + " feet tall.");
+Console.WriteLine("He's " + (myWeight/3.7) + " Kg heavy.");
+Console.WriteLine("Actually that's not too heavy");
+Console.WriteLine("He's got " + myEyes + " eyes and " + myHair +" hair.");
+Console.WriteLine("His teeth are " + myTeeth);
+Console.WriteLine("If I add " + myAge + " , " + myHeight + " , and " + myWeight + " I get " + (myAge + myHeight + myWeight));
+
+string name = ("Raine");
+Console.WriteLine(name);
+name = ("DeVries");
+Console.WriteLine(name);
+
+int number = 22;
+Console.WriteLine(number);
+
+int myNumber = 15;
+myNumber = 15;
+Console.WriteLine(myNumber);
+
+double height = 5.7;
+Console.WriteLine(height);
+
+Console.WriteLine("The difference between int and double data type is int will store integer values and double stores decimal values");
+Console.WriteLine("yes if using variables");
+int age = 15;
+Console.WriteLine(age / 9.0);
+Console.WriteLine("it gets overwriten");
+
+
